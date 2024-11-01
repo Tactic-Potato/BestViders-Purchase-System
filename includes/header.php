6bd682ddfa388a4caa2838f4092d64e329fd7d2f
Lara <jilljosue95@gmail.com>
2024-10-31 22:09:30 -0700

    mas tablas

--- includes/header.php
+++ includes/header.php
@@ -15,17 +15,27 @@
                 <li><a href="index.php">Home</a></li>
                     <li><a>Providers</a>
                         <ul>
-                            <li><a href="providers.php">Check Providers</a></li>
+                            <li><a href="providers.php">View Providers</a></li>
                         </ul>
                     </li>
                     <li><a>Materials</a>
                         <ul>
-                            <li><a href="materials.php">Materials</a></li>
+                            <li><a href="materials.php">Check Materials</a></li>
                         </ul>
                     </li>
                     <li><a>Orders</a>
                         <ul>
-                            <li><a href="order.php">Orders</a></li>
+                            <li><a href="order.php">Check Orders</a></li>
+                        </ul>
+                    </li>
+                    <li><a>Request</a>
+                        <ul>
+                            <li><a href="request.php">Chech Requests</a></li>
+                        </ul>
+                    </li>
+                    <li><a>Employee</a>
+                        <ul>
+                            <li><a href="employee.php">View employees</a></li>
                         </ul>
                     </li>
                 </ul>
---
+++ request.php
@@ -0,0 +1,30 @@
+<?php  include "includes/header.php";?>
+<div id="infRequest">
+    <center>
+        <table>
+            <tr>
+                <th>Number</th>
+                <th>Status</th>
+                <th>Subtotal</th>
+                <th>Date</th>
+                <th>Check by Employee</th>
+                <th>Provider</th>
+            </tr>
+            <?php 
+                include "includes/config/conn.php";
+                $db=connect();
+                $query = mysqli_query($db,  "SELECT * FROM REQUEST");
+                while ($result = mysqli_fetch_array($query)){ ?>
+                    <tr>
+                        <td><?=$result['num']?></td>
+                        <td><?=$result['status']?></td>
+                        <td><?=$result['subtotal']?></td>
+                        <td><?=$result['requestDate']?></td>
+                        <td><?=$result['employee']?></td>
+                        <td><?=$result['provider']?></td>
+                    </tr>
+                <?php } mysqli_close($db); ?>
+        </table>
+    </center>
+</div>
+<?php include "includes/footer.php"?>
