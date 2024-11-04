46e77a110f2284c6f008b7cf61a0a88b4d14dea8
Lara <jilljosue95@gmail.com>
2024-11-04 11:42:38 -0800

    ya

--- providerProcess.php
+++ providerProcess.php
@@ -9,8 +9,11 @@ $numTel = $_POST['numTel'];
 
 $insert = "INSERT INTO provider (num, fiscalName, email, numTel) VALUES ('$num','$fiscalName', '$email','$numTel')";
 
-if()
-
+if($conn->query($insert) === TRUE){
+    echo '<script type="text/javascript"> alert("Registro Exitoso"); window.location.href="createProvider.php" </script>';
+}else{
+    echo '<script type="text/javascript"> alert("Error al registrar"); window.location.href="createProvider.php" </script>';
+}
 
 
 $conn ->close();
