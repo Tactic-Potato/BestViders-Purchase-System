68a11154316d8483b979d555ab1f21852464d0fc
Lara <jilljosue95@gmail.com>
2024-11-05 19:58:17 -0800

    cambios en el css

--- createProvider.php
+++ createProvider.php
@@ -7,26 +7,27 @@ $row = $result->fetch_assoc();
 $last_id = $row['last_id'] + 1;
 ?>
 
-<link rel="stylesheet" href="incldues/css/forms.css">
+<link rel="stylesheet" href="includes/css/forms.css">
 <nav id="Return"><a href="index.php"> Return </a></nav>
-<h2>ADD PROVIDER</h2>
 <section id="formCont">
     <div id="formCard">
-        <legend>Fill all fields</legend>
-        <label>Provider Number</label>
-        <input type="number" name="num" id="num" value="<?php echo $last_id;?>" required readonly>
+        <form action="providerProcess.php" method="POST">
+            <legend>Fill all fields</legend>
+            <label>Provider Number</label>
+            <input type="number" name="num" id="num" value="<?php echo $last_id;?>" required readonly>
 
-        <label>Fiscal Name</label>
-        <input type="text" name="fiscalName" id="fiscalName" required>
-        
-        <label>Phone Number</label>
-        <input type="text" name="numTel" id="numTel" required>
-    
-        <label>Email</label>
-        <input type="email" name="email" id="email" required>
+            <label>Fiscal Name</label>
+            <input type="text" name="fiscalName" id="fiscalName" required>
+            
+            <label>Phone Number</label>
+            <input type="text" name="numTel" id="numTel" required>
 
-        <div>
-            <button type="submit" class="button"> ADD </button>
-        </div>
+            <label>Email</label>
+            <input type="email" name="email" id="email" required>
+
+            <div>
+                <button type="submit" class="button"> ADD </button>
+            </div>
+        </form>
     </div>
 </section>
--- providerProcess.php
+++ providerProcess.php
@@ -10,11 +10,29 @@ $numTel = $_POST['numTel'];
 $insert = "INSERT INTO PROVIDER (num, fiscalName, email, numTel) VALUES ('$num','$fiscalName', '$email','$numTel')";
 
 if($conn->query($insert) === TRUE){
-    echo '<script type="text/javascript"> alert("Registro Exitoso"); window.location.href="createProvider.php" </script>';
-}else{
-    echo '<script type="text/javascript"> alert("Error al registrar"); window.location.href="createProvider.php" </script>';
+    echo '
+        <link rel="stylesheet" href="includes/css/forms.css">
+        <script type="text/javascript">
+            document.addEventListener("DOMContentLoaded", function() {
+                document.getElementById("customModal").style.display = "block";
+            });
+        </script>
+        
+        <!-- Modal HTML -->
+        <div id="customModal" class="modal">
+            <div class="modal-content">
+                <p>Registro Exitoso. ¿Desea registrar de nuevo?</p>
+                <button onclick="window.location.href=\'createProvider.php\'" class="modal-button">Sí</button>
+                <button onclick="window.location.href=\'index.php\'" class="modal-button">No</button>
+            </div>
+        </div>
+    ';
+} else {
+    echo '<script type="text/javascript">
+            alert("Error al registrar");
+            window.location.href = "createProvider.php";
+          </script>';
 }
 
-
-$conn ->close();
+$conn->close();
 ?>
