a2827bae932cac498166624e04b6ec4c9a17c21e
Lara <jilljosue95@gmail.com>
2024-12-03 21:18:23 -0800

    ei

--- provider/rehireProvider.php
+++ provider/rehireProvider.php
@@ -14,7 +14,7 @@ if (!$infoProvider) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rehire Provider</title>
